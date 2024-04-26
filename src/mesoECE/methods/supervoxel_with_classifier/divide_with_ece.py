2cbeca251b1fcb7824ef16432b6e70a4c6d41fea
Taylla Theodoro <tayllamth@gmail.com>
2024-04-26 01:15:04 +0000

    structuring:  div with ece from recursive to iterative

--- src/mesoECE/methods/supervoxel_with_classifier/divide_with_ece.py
+++ src/mesoECE/methods/supervoxel_with_classifier/divide_with_ece.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import skimage
 from skimage.segmentation import slic
@@ -8,8 +10,9 @@ from src.mesoECE.methods.superspels.utils import define_mean_intensity_curves, \
     save_curves_and_interp_to_csv, save_superspels_masks, plot_curves
 
 from src.mesoECE.methods.utils import (define_masks_volume,
-                                       correct_images_background)
+                                       correct_images_background, setup_directories)
 from src.mesoECE.methods.classifier.utils import superspels_labels_with_ece
+import nibabel as nib
 
 
 class DivideWithECE(AbstractMethod):
@@ -38,33 +41,19 @@ class DivideWithECE(AbstractMethod):
     def apply(self, patient: Patient, **kwargs):
         try:
             print(f"Processing patient {patient.id}...")
-            path_m_images = self.path_sv / 'ece_images'
-            path_b_images = self.path_sv / 'benign_images'
-            path_plot = self.path_sv / 'plots'
-            path_df = self.path_sv / 'curves_df'
-
-            pleural_mask = patient.get_image(self.ref_t).masks[
-                "pleural_region"].data.astype(np.int32)
-
+            setup_directories(path=self.path_sv,
+                              dir_names=['ece_images', 'benign_images',
+                                         'plots', 'curves_df'])
+            pleural_mask = patient.get_image(self.ref_t).masks["pleural_region"].data.astype(np.int32)
             image = patient.get_image(self.ref_t).data
             nifti_args = patient.get_image(self.ref_t).nifti_props
-            image_header = nifti_args["header"]
-            self.mri_spacing = image_header.get_zooms()
+            self.mri_spacing = nifti_args["header"].get_zooms()
 
-            self.p_vol = define_masks_volume(pleural_mask)
-
-
+            # Process the segmentation based on the `predict_only_small` flag
             if self.predict_only_small:
-                self.divide_with_ece_small_slic(patient=patient,
-                                                image=image,
-                                                mask=pleural_mask,
-                                                n_segments=self.n_segments)
+                self.segment_and_classify(patient, image, pleural_mask, self.n_segments, classify_immediately=False)
             else:
-                self.divide_with_ece_slic(patient=patient,
-                                          image=image,
-                                          mask=pleural_mask,
-                                          n_segments=self.n_segments)
-
+                self.segment_and_classify(patient, image, pleural_mask, self.n_segments, classify_immediately=True)
             sv_m_mask, sv_b_mask = self.combine_supervoxel_masks(
                 patient,
                 pleural_mask)
@@ -79,32 +68,33 @@ class DivideWithECE(AbstractMethod):
                      patient.subclass_diagnosis, patient.nodular,
                      n_supervoxel, self.s_vol])
 
-                # ece_curves = define_mean_intensity_curves(
-                #     patient=patient,
-                #     mask=sv_m_mask,
-                #     images_corrected=self.images_corrected,
-                #     domain=self.domain)
+                ece_curves, std_ece_curves = define_mean_intensity_curves(
+                    patient=patient,
+                    mask=sv_m_mask,
+                    domain=self.domain)
 
-                # patient.curves['ece'] = ece_curves
-                #
-                # save_curves_and_interp_to_csv(patient=patient,
-                #                               curves=ece_curves,
-                #                               path=path_df,
-                #                               curve_name='ece')
-                #
+                save_curves_and_interp_to_csv(patient=patient,
+                                              curves=std_ece_curves,
+                                              path=self.path_sv / 'curves_df',
+                                              curve_name='ece')
+                save_curves_and_interp_to_csv(patient=patient,
+                                              curves=ece_curves,
+                                              path=self.path_sv / 'curves_df',
+                                              curve_name='ece')
+
                 # # Plot mean intensity curves
-                # plot_curves(curve=ece_curves,
-                #             time_points=patient.time_points,
-                #             mask=sv_m_mask,
-                #             filename=str(path_plot / f'ece_{patient.id}.png'),
-                #             mean_plot=True)
-                #
-                # save_superspels_masks(mask=sv_m_mask,
-                #                       nifti_args=patient.nifti_args,
-                #                       patient=patient,
-                #                       domain=self.domain,
-                #                       ref_t=self.ref_t,
-                #                       path=path_m_images)
+                plot_curves(curve=ece_curves,
+                            time_points=patient.time_points,
+                            mask=sv_m_mask,
+                            filename=str(self.path_sv / 'plots' / f'ece_{patient.id}.png'),
+                            mean_plot=True)
+
+                # Save supervoxels mask
+                nib.save(nib.Nifti1Image(sv_m_mask.astype(np.int32),
+                                         **nifti_args),
+                         str(self.path_sv / 'ece_images' / patient.get_image(
+                             self.ref_t).filename))
+
 
             else:
                 self.predicted_diagnosis.append(
@@ -114,35 +104,37 @@ class DivideWithECE(AbstractMethod):
 
             # # save benign supervoxels
             #
-            # benign_curves = define_mean_intensity_curves(
-            #     patient=patient,
-            #     mask=sv_b_mask,
-            #
-            #     domain=self.domain)
+            benign_curves, std_ece_curves = define_mean_intensity_curves(
+                patient=patient,
+                mask=sv_b_mask,
+                domain=self.domain)
 
             # patient.curves['benign'] = benign_curves
-            # save_curves_and_interp_to_csv(patient=patient,
-            #                               curves=benign_curves,
-            #                               path=path_df,
-            #                               curve_name='benign')
-            # save_superspels_masks(mask=sv_b_mask,
-            #                       nifti_args=patient.nifti_args,
-            #                       patient=patient,
-            #                       domain=self.domain,
-            #                       ref_t=self.ref_t,
-            #                       path=path_b_images)
-            #
-            # plot_curves(curve=benign_curves,
-            #             time_points=patient.time_points,
-            #             mask=sv_b_mask,
-            #             filename=str(path_plot / f'benign_{patient.id}.png'),
-            # #             mean_plot=True,
-            #             title='Benign Curves')
+            save_curves_and_interp_to_csv(patient=patient,
+                                          curves=benign_curves,
+                                          path=self.path_sv / 'curves_df',
+                                          curve_name='benign')
+            save_curves_and_interp_to_csv(patient=patient,
+                                          curves=std_ece_curves,
+                                          path=self.path_sv / 'curves_df',
+                                          curve_name='std_benign')
+            # Save supervoxels mask
+            nib.save(nib.Nifti1Image(sv_b_mask.astype(np.int32),
+                                     **nifti_args),
+                     str(self.path_sv / 'benign_images' / patient.get_image(
+                         self.ref_t).filename))
 
-            patient.path_masks['supervoxels_with_ece'] = self.path_sv
+            plot_curves(curve=benign_curves,
+                        time_points=patient.time_points,
+                        mask=sv_b_mask,
+                        filename=str(self.path_sv / 'plots' / f'benign_{patient.id}.png'),
+                        mean_plot=True,
+                        title='Benign Curves')
 
-        except:
-            print("Error in patient:", patient.id)
+
+        except Exception as e:
+            print(f"Error processing patient {patient.id}: {e}")
+
         new_patient = Patient(path=patient.path,
                               path_masks=patient.path_masks,
                               id=patient.id,
@@ -154,95 +146,37 @@ class DivideWithECE(AbstractMethod):
     def result(self):
         return self.predicted_diagnosis
 
-    def divide_with_ece_small_slic(self, patient: Patient, image, mask,
-                                   n_segments):
-
-        if define_masks_volume(mask) > self.p_size:
-
-            print("\r", end='')
-            print("Dividing...", end="", flush=True)
-            print(define_masks_volume(mask))
-
-            supervoxel_mask = slic(image=image,
-                                   mask=mask,
-                                   compactness=self.compactness,
-                                   n_segments=n_segments, channel_axis=None,
-                                   start_label=1,
-                                   spacing=self.mri_spacing)
-            rps = skimage.measure.regionprops(supervoxel_mask)
-
-
-            for rp in rps:
-                div_mask = np.zeros_like(mask)
-
-                slice_bbox = tuple(
-                    [slice(dim_start, dim_finish) for dim_start, dim_finish in
-                     zip(rp.bbox[:3], rp.bbox[3:])])
-                lbl_in_bbox = rp.image
-                div_mask[slice_bbox] = lbl_in_bbox
-
-                self.divide_with_ece_small_slic(patient, image, div_mask,
-                                                n_segments)
-
-        else:
-            if self.predict_ece(patient, mask) == 1:
-                return patient.supervoxels_m_masks.append(mask)
+    def segment_and_classify(self, patient, image, mask, n_segments, classify_immediately):
+        stack = [(image, mask)]
+        while stack:
+            img, msk = stack.pop()
+            if define_masks_volume(msk) <= self.p_size or classify_immediately:
+                # Direct classification and storage decision based on predict_ece outcome
+                diagnosis = self.predict_ece(patient, msk)
+                if diagnosis == 1:
+                    patient.supervoxels_m_masks.append(msk)
+                else:
+                    patient.supervoxels_b_masks.append(msk)
             else:
-                return patient.supervoxels_b_masks.append(mask)
+                # Perform further segmentation
+                supervoxel_mask = slic(image=img, mask=msk, compactness=self.compactness,
+                                       n_segments=n_segments, channel_axis=None, start_label=1,
+                                       spacing=self.mri_spacing)
+                rps = skimage.measure.regionprops(supervoxel_mask)
+                for rp in rps:
+                    div_mask = np.zeros_like(msk)
+                    slice_bbox = tuple(
+                        slice(dim_start, dim_finish) for dim_start, dim_finish in zip(rp.bbox[:3], rp.bbox[3:]))
+                    lbl_in_bbox = rp.image
+                    div_mask[slice_bbox] = lbl_in_bbox
+                    stack.append((img, div_mask))
 
-    def divide_with_ece_slic(self, patient: Patient, image, mask,
-                             n_segments):
-        if (self.predict_ece(patient, mask) == 0 or
-                define_masks_volume(mask) > self.p_size):
-            print("\r", end='')
-            print("Dividing...", end="", flush=True)
-
-            supervoxel_mask = slic(image=image,
-                                   mask=mask,
-                                   compactness=self.compactness,
-                                   n_segments=n_segments, channel_axis=None,
-                                   start_label=1,
-                                   spacing=self.mri_spacing)
-            rps = skimage.measure.regionprops(supervoxel_mask)
-
-            for rp in rps:
-                div_mask = np.zeros_like(mask)
-
-                slice_bbox = tuple(
-                    [slice(dim_start, dim_finish) for dim_start, dim_finish in
-                     zip(rp.bbox[:3], rp.bbox[3:])])
-                lbl_in_bbox = rp.image
-                div_mask[slice_bbox] = lbl_in_bbox
-
-                self.divide_with_ece_small_slic(patient, image, div_mask,
-                                                n_segments)
-
-        elif self.predict_ece(patient, mask) == 1:
-            return patient.supervoxels_m_masks.append(mask)
-        elif (self.predict_ece(patient, mask) == 0 and
-              define_masks_volume(mask) < self.p_size):
-            return patient.supervoxels_b_masks.append(mask)
-
-    def predict_ece(self, patient: Patient, supervoxel_mask):
-
-        mean_intensity_curve = define_mean_intensity_curves(
-            patient=patient,
-            mask=supervoxel_mask,
-            domain=self.domain)
-
-        ece_labels = superspels_labels_with_ece(
-            index_270=patient.time_points.index(270),
-            mean_intensity_curve=mean_intensity_curve)
-
-        s_vol = define_masks_volume(mask=supervoxel_mask)
-
-        if len(ece_labels) > 0 and s_vol > self.p_vol * 0.0001:
-            predicted_diagnosis = 1
-
-        else:
-            predicted_diagnosis = 0
-
-        return predicted_diagnosis
+    def predict_ece(self, patient, mask):
+        curves = define_mean_intensity_curves(patient=patient, mask=mask, domain=self.domain)
+        ece_labels = superspels_labels_with_ece(index_270=patient.time_points.index(270),
+                                                mean_intensity_curve=curves[0])
+        s_vol = define_masks_volume(mask)
+        return 1 if len(ece_labels) > 0 and s_vol > self.p_size * 0.0001 else 0
 
     @staticmethod
     def combine_supervoxel_masks(patient: Patient, pleural_mask):
--- src/mesoECE/methods/utils.py
+++ src/mesoECE/methods/utils.py
@@ -1,7 +1,13 @@
+import os
+from pathlib import Path
+
 import numpy as np
 from src.mesoECE.data_structure import Patient
 
 
+def setup_directories(path: Path, dir_names: list):
+    for directory in dir_names:
+        os.makedirs(path / directory, exist_ok=True)
 def define_masks_volume(mask):
     mask_volume = np.count_nonzero(mask)
     return mask_volume
